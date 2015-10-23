6f443e41348877fd8b7103bb29bf0a767757a4e6
Peter Petrovich <petro@petro.ws>
2015-10-23 16:36:52 +0300

    table fix

--- include/class.ltable.php
+++ include/class.ltable.php
@@ -1,9 +1,9 @@
 <?php
 /** @file
- * LTable class. 
- * 
- * Written by Peter, 
- * mailto:petro@petro.ws 
+ * LTable class.
+ *
+ * Written by Peter,
+ * mailto:petro@petro.ws
  * http://petro.ws/projects/web/php-light-classes/
  */
 
@@ -23,19 +23,30 @@
 	public $class_row_sep	= 'tablegen_sep';	# Row class separator
 	public $td_def_align	= 'center';		# Default align in TD
 	public $td_def_valign	= 'middle';		# Default valign in TD
-	
+
 	public $bg 		= true;			# Has border/background DIV tag?
 
 	public $th_top		= true;			# Add titles to Top
 	public $th_bottom	= true;			# Add titles to Bottom
 
 	public $noutems_auto	= true;			# Add 'No items to show' auto, when no items added
-	
+
 	# Internal
 	private $table_data = array ();
 	private $table_th_data = array ();
 	private $tabe_tr_params = array ();
-	
+
+	function add_row($data)
+	{
+		$new_data = array ();
+		foreach ($data as $param)
+		{
+			$new_data[] = array ('data'=>$param);
+		}
+
+		$this->table_data[] = $new_data;
+	}
+
 	function add_row_simple ()
 	{
 		$params = func_get_args();
@@ -52,12 +63,12 @@
 	{
 		$this->table_data[][0] = array ('data'=>$text,'colspan'=>count($this->table_th_data),'class'=>$this->class_row_sep);
 	}
-	
+
 	function add_tr_params ($params)
 	{
 		$this->table_tr_params[count($this->table_data)-1] = $params;
 	}
-	
+
 	function add_th ($title, $width=false, $align=false, $class=false)
 	{
 		$new_th = array ();
@@ -68,17 +79,17 @@
 			$new_th['align'] = $align;
 		if ($class)
 			$new_th['class'] = $class;
-			
+
 		$this->table_th_data[] = $new_th;
 	}
-	
+
 	function add_no_items ($text=false, $tags=true)
 	{
-		if (!$text && function_exists('lang')) $text = lang('noitems');
+		if (!$text && function_exists('lang')) $text = 'No items';
 		if ($tags)	$text = '<div style="text-align:center;padding:15px;">'.$text.'</div>';
 		$this->table_data[][] = array ('data'=>$text, 'colspan'=>count($this->table_th_data), 'align'=>'center');
 	}
-	
+
 	function get_table ()
 	{
 		if ($this->noutems_auto && !count($this->table_data))
@@ -88,15 +99,15 @@
 		# Background ?
 		if ($this->bg)
 			$out .= '<table border="0" cellpadding="0" cellspacing="0" width="'.$this->width.'"><tr><td class="'.$this->class_bg.'">'."\n";
-			
+
 		# Fill The Header
 		$out .= '<table border="'.$this->border.'" width="'.$this->width.'" cellpadding="'.$this->pad.'" cellspacing="'.$this->sp.'"';
 		if ($this->class_table) $out .= ' class="'.$this->class_table.'"';
 		if ($this->table_style) $out .= ' style="'.$this->table_style.'"';
 		$out .= " >\n";
-		
+
 		if ($this->th_top) $out .= $this->_get_th();
-		
+
 		for ($x=0; $x<count($this->table_data); $x++)
 		{
 			$row = $this->table_data[$x];
@@ -115,7 +126,7 @@
 			if (@$this->table_tr_params[$x])
 				foreach ($this->table_tr_params[$x] as $k=>$v)
 					$row_out .= ' '.$k.'="'.$v.'"';
-					
+
 			$row_out .= ">\n";
 			$row_out = str_replace ('{ROW_CLASS}', @$this->table_tr_params[$x]['class'], $row_out);
 
@@ -134,7 +145,7 @@
 				$row_out .= "\t\t".@$row[$i]['data'];
 				$row_out .= "\n\t</td>\n";
 			}
-			
+
 			$row_out .= "</tr>\n";
 			$out .= $row_out;
 		}
@@ -145,11 +156,11 @@
 		$out .= "\n";
 		return $out;
 	}
-	
+
 	function _get_th ()
 	{
 		if (!$this->table_th_data) return;
-		
+
 		$out = '<tr class="'.$this->class_row_th.'">'."\n";
 		for ($x=0; $x<count($this->table_th_data); $x++)
 		{
